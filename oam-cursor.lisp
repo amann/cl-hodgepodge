9f32e0af640238eaaae318fe08c4cbf6648fa878
Olivier <olivier.amann@swisslife.ch>
2010-12-23 17:30:03 +0100

    Work in progress: Refining the initialization process of etl:table.

--- oam-cursor.lisp
+++ oam-cursor.lisp
@@ -7,23 +7,20 @@
 ;;;; `no-next-element-error' must be thrown or an eof object returned.
 (in-package #:cl-user)
 (defpackage #:ch.amann-wolowyk.cursor
-  (:nicknames #:cursor)
-  (:use #:common-lisp #:oam)
-  (:export #:no-next-element-error
-           #:map-cursors
-           #:make-cursor
-           #:make-list-cursor
-           #:make-mumber-cursor))
-(in-package #:cursor)
+  (:use)
+  (:nicknames #:cursor))
+(defpackage #:ch.amann-wolowyk.cursor-system
+  (:use #:common-lisp))
+(in-package #:ch.amann-wolowyk.cursor-system)
 
-(define-condition no-next-element-error (error)
+(define-condition cursor::no-next-element-error (error)
   ((cursor :initarg :cursor))
   (:report (lambda (condition stream)
              (format stream "There is no next element for cursor ~A."
                      (slot-value condition 'cursor))))
   (:documentation "Condition of supertype `error' which is signaled by the cursor when no next element can be generated."))
 
-(defun map-cursors (type fn cursor &rest cursors)
+(defun cursor::map-cursors (type fn cursor &rest cursors)
   "Map the output of CURSORS to the function FN and return a sequence of the type TYPE containing the results of the mapping. If TYPE is nil, nil is returned. The loop terminates as soon as one of the cursors throws a `no-next-element-error'. It is therefore expected that (at least one of) the cursors throws a `no-next-element-error' to terminate the loop. TYPE is one of nil, list, vector or string."
   (let ((cursors (cons cursor cursors)))
     (ecase type
@@ -53,10 +50,10 @@
              collection)))))))
 
 
-(defgeneric make-cursor (output-type object &key eof &allow-other-keys)
+(defgeneric cursor::make-cursor (object &key eof &allow-other-keys)
   (:documentation "Return a cursor taking OBJECT as base and returning EOF if non nil or throwing a `no-next-element-error' if EOF is nil. A cursor is a (anonymous) function which when successively called returns a new object. The specializer OUTPUT-TYPE may be used to specify the output type by implementing an adequate conversion."))
 
-(defun make-list-cursor (list &key (step 1) key eof)
+(defun cursor::make-list-cursor (list &key (step 1) key eof)
   (let* ((list list)
          (step (if (numberp step)
 		   step
@@ -77,7 +74,7 @@
                       `(prog1 ,get
                          ,next)))))))
 
-(defun make-number-cursor (n &key to (step 1) (eof nil))
+(defun cursor::make-number-cursor (n &key to (step 1) (eof nil))
   "Return a cursor producing numbers from N below TO by steps STEP. If TO <= N and 0 < STEP or N <= TO and STEP < 0 or if TO is not a number the cursor never terminates. If STEP is not a number it is set to 1. A STEP of 0 produces an infinite sequence of the constant N (Not really useful). If N is no a number it is set to 0. If EOF is not nil it is returned when reaching the terminating condition is reached instead of throwing a `no-next-element-error'."
   (let* ((n (if (numberp n)
 		n
@@ -104,7 +101,16 @@
                       `(prog1 ,get
                          ,next)))))))
 
-(defmethod make-cursor ((n number) &key eof (step 1) to &allow-other-keys)
-  (make-number-cursor n :to to :step step :eof eof))
-(defmethod make-cursor ((list list)&key eof (step 1) key &allow-other-keys)
-  (make-list-cursor list :step step :key key :eof eof))
+(defmethod cursor::make-cursor ((n number) &key eof (step 1) to
+                                &allow-other-keys)
+  (cursor::make-number-cursor n :to to :step step :eof eof))
+(defmethod cursor::make-cursor ((list list)&key eof (step 1) key
+                                &allow-other-keys)
+  (cursor::make-list-cursor list :step step :key key :eof eof))
+
+
+
+(let ((package (find-package '#:ch.amann-wolowyk.cursor)))
+  (do-symbols (symbol package)
+    (when (eq (symbol-package symbol) package)
+      (export symbol package))))
--- oam-etl.lisp
+++ oam-etl.lisp
@@ -33,28 +33,34 @@
   ((package :reader etl::package)
    (tables :initform nil)))
 
+(defmethod slot-unbound (class (instance etl::datapool) (slot-name (eql 'package)))
+  nil)
+(defmethod slot-unbound (class (instance etl:datapool) (slot-name (eql 'tables)))
+  nil)
 (let ((datapools (make-hash-table :test #'eq)))
-  (defmethod shared-initialize :after ((self etl::datapool) slot-names &key name
-                                         &allow-other-keys)
+  (defmethod shared-initialize :after ((self etl::datapool) slot-names
+                                       &key name &allow-other-keys)
     (declare (ignore slot-names))
     (let* ((package (make-package name)))
       (setf (slot-value self 'package) package
             (gethash package datapools) self)))
-  (defmethod reinitialize-instance :before ((self etl::datapool) &key
-                                            &allow-other-keys)
-    (etl::delete-datapool self))
-  (defun find-datapool (datapool)
+  (defun etl::find-datapool (datapool)
     (etypecase datapool
       (etl::datapool
        (setf (gethash (etl::package datapool) datapools) datapool))
       (package (nth-value 0(gethash datapool datapools)))
-      ((or symbol string) (find-datapool (find-package datapool)))))
-  (defun delete-datapool (datapool)
+      ((or symbol string) (etl::find-datapool (find-package datapool)))))
+
+  (defun etl::delete-datapool (datapool)
     (with-slots (package tables) datapool
-      (remhash package datapools)
       (map nil #'delete-table tables)
-      (delete-package package))
-    (slot-makunbound datapool 'package)))
+      (when (packagep package)
+        (remhash package datapools)
+        (delete-package package)))
+    (slot-makunbound datapool 'package))
+  (defmethod reinitialize-instance :before ((self etl::datapool) &key
+                                            &allow-other-keys)
+    (etl::delete-datapool self)))
 
 
 
@@ -76,29 +82,49 @@
 ;;;; A table is a list of rows, is given a name and associated to a datapool.
 (defvar etl::*row-name* "@ROW@")
 (defclass etl::table ()
-  (rows
+  ((rows :initform (error "No data given.") :initarg :rows)
    (datapool :initform (error "No datapool given.")
              :type etl::datapool :reader etl::datapool)
-   (name :initform (error "No name given.")
-         :type symbol :initarg :name :reader etl::name)
-   (package :reader etl::package)
-   (column-names :documentation "List of symbols naming the columns of the table. The symbols are in the package DATABASE-NAME/TABLE-NAME.")))
+   (name :initform (error "No name given!")
+         :type symbol :reader etl::name)
+   (row-name :type symbol :reader etl::row-name)
+   (package :type package :reader etl::package)
+   (column-names :type list :reader etl::column-names
+    :documentation "List of symbols naming the columns of the table. The symbols are in the package DATAPOOL-NAME/TABLE-NAME.")))
 
-
+;;;; TODO: Provide restarts in order to change the names if a conflict appears with the package name.
+(defun ensure-table-package (datapool-name table-name)
+  (restart-case
+      (values (make-package (format nil "~A/~A"
+                                    datapool-name table-name)
+                            :use nil)
+              datapool-name table-name)
+    (use-new-table-name (new-table-name)
+      :report "Use new table name."
+      :interactive (lambda ()
+                     (format t "Enter a new value: ")
+                     (multiple-value-list (eval (read))))
+      (ensure-table-package datapool-name new-table-name))
+    (use-new-datapool-name (new-datapool-name)
+      :report "Use new table name."
+      :interactive (lambda ()
+                     (format t "Enter a new value: ")
+                     (multiple-value-list (eval (read))))
+      (ensure-table-package datapool-name new-table-name))))
 (defmethod shared-initialize :before ((self etl::table) slot-names
                                       &key name datapool &allow-other-keys)
   (declare (ignore slot-names))
   (let* ((datapool (setf (slot-value self 'datapool)
-                         (or (find-datapool datapool)
+                         (or (etl::find-datapool datapool)
                              (error "No such datapool: ~S." datapool))))
          (db-package (etl::package datapool))
          (name (if name
-                   (export (intern (string name) db-package) db-package)
+                   (let ((name (intern (string name) db-package)))
+                     (export name db-package)
+                     (setf (slot-value self 'name) name))
                    (error "No name given."))))
-    (setf (slot-value self 'package)
-          (make-package (format nil "~A/~A"
-                                (package-name db-package) name)
-                        :use nil))))
+    ( (slot-value self 'package)
+          )))
 (defmethod shared-initialize :after ((self etl::table) slot-names
                                      &key column-names &allow-other-keys)
   (declare (ignore slot-names))
@@ -112,29 +138,31 @@
     (let ((name (etl::name self))
           (row-name (intern etl::*row-name* package)))
       (export row-name package)
+      (setf (slot-value self 'row-name) row-name)
       (etl::make-symbol-binder name row-name column-names)
       (set name self))))
 
-
-(defun delete-table (table)
-  (remove table (slot-value (etl::datapool table) 'tables)
-          :test #'eq)
+(defun etl::delete-table (table)
+  (unless (slot-unboundp table 'datapool)
+    (with-slots (tables) (etl:datapool table)
+      (setf tables (delete table tables :test #'eq))))
   (let ((package (etl::package table)))
     (unintern (etl::name table) package)
     (delete-package package))
-  (slot-makunbound table 'package)
-  (slot-makunbound table 'column-names))
+  (map nil #'(lambda (slot) (slot-makunbound table (ccl:slot-definition-name slot)))
+       (ccl:class-slots (class-of table))))
 
 (defmethod reinitialize-instance :before ((self etl::table)
                                           &key &allow-other-keys)
-  (delete-table self))
+  (etl::delete-table self))
 
 
-(defgeneric etl::nth-row (object output-type))
+(defgeneric etl::nth-row (index object))
 
-(defmethod cursor::make-cursor (output-type (object etl::table)
-                                &key eof (step 1) key &allow-other-keys)
-  (cursor::make-cursor (slot-value object 'rows) output-type
+(defmethod cursor::make-cursor ((object etl::table)
+                                &key eof (step 1) key
+                                &allow-other-keys)
+  (cursor:make-cursor (slot-value object 'rows)
                        :step step :key key :eof eof))
 
 (defmacro etl::do-table-rows (table-name &body body)
@@ -142,16 +170,84 @@
   (let ((rsmap (gensym "RSMAP"))
         (next-row-fn (gensym "NEXT-ROW-FN")))
     `(let* ((,rsmap (,table-name ,@body))
-            (,next-row-fn (cursor::make-cursor (find-class 'list)
-                                               ,table-name)))
+            (,next-row-fn (cursor:make-cursor ,table-name)))
        (handler-case
-           (loop (apply ,rsmap (funcall ,next-row-fn)))
+           (loop (apply ,rsmap (etl::to-list (funcall ,next-row-fn))))
          (cursor:no-next-element-error ())))))
 
+(labels
+    ((get-map (hash-table columns)
+       (if columns
+           (handler-case
+               (get-map (gethash hash-table (first columns)) (rest 
+                                                              columns))
+             (type-error ()))
+           hash-table))
+     (add-map (hash-table column value rest-columns)
+       (if rest-columns
+           (destructuring-bind (col test) (first rest-columns)
+             (add-map (or (gethash (funcall column value) hash-table)
+                          (setf (gethash (funcall column value) hash-table)
+                                (make-hash-table :test test)))
+                      col value (rest rest-columns)))
+           (push value (gethash column hash-table)))))
+  (defmacro etl::define-table-index (name (&rest columns) table)
+    "Define an index on the columns given in COLUMNS of table TABLE."
+    (let* ((columns (mapcar #'(lambda (col)
+                                (etypecase col
+                                  (symbol (list col #'eql))
+                                  (cons col)))
+                            columns))
+           (args (mapcar #'car columns)))
+      `(let* ((table ,table)
+              (row-name (etl::row-name table))
+              (hash-table (make-hash-table :test ,(cadar columns))))
+         (defun ,name (,@args)
+           (funcall ,#'get-map hash-table (list ,@args)))
+         (etl::do-table-rows ,table
+           (funcall ,#'add-map hash-table ,(caar columns) row-name ',(rest columns)))))))
 
 
-#+(or)
-(let ((package (find-package #:oam)))
+#+ (or)
+(labels
+    ((get-map (hash-table columns)
+       (handler-case
+           (get-map (gethash hash-table (first columns)) (rest columns))
+         (type-error ())))
+     (add-map (hash-table columns value test)
+       (if (= 1 (length columns))
+           (push value (gethash (first columns) hash-table))
+           (add-map (let ((col (first columns)))
+                      (or (gethash col hash-table)
+                          (setf (gethash col hash-table)
+                                (make-hash-table :test test))))
+                    (rest columns) test))))
+  (defmacro define-table-index (name (&rest columns) table)
+    "Define an index on the columns given in COLUMNS of table TABLE."
+    (let* ((tbl (gensym "TABLE")))
+      `(let ((,tbl ,table)
+             (hash-table (make-hash-table))) 
+         (defun ,name (,@(mapcar #'(lambda (col)
+                                      (etypecase col
+                                        (symbol col)
+                                        (cons (first col))))
+                                 columns))
+           (funcall #'get-map ))))))
+
+
+
+
+(defgeneric etl::to-list (object &key &allow-other-keys))
+(defmethod etl::to-list ((object list) &key key &allow-other-keys)
+  (if key
+      (call-next-method)
+      object))
+(defmethod etl::to-list ((object sequence) &key key &allow-other-keys)
+  (map 'list (or key #'identity) object))
+
+(defgeneric etl::map-from-to (from-domain to-domain value))
+
+(let ((package (find-package '#:etl)))
   (do-symbols (symbol package)
     (when (eq (symbol-package symbol) package)
       (export symbol package))))
