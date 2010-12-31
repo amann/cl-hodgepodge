b85f8de04942bb27004e991e6bbcea3d608110ad
Olivier Amann <olivier.e.amann@gmail.com>
2010-12-31 23:43:56 +0100

    Corrected author name and email.

--- oam-sxml.lisp
+++ oam-sxml.lisp
@@ -1,7 +1,6 @@
 ;;;; -*- outline-regexp:";;;;[*]+ +" -*-
 ;;;;$Id: oam-sxml.lisp,v 1.1 2010/08/26 11:25:42 amao Exp $
-(eval-when (:compile-toplevel :load-toplevel :execute)
-           (asdf:oos 'asdf:load-op 's-xml))
+
 ;;;;* Extensions to S-XML
 ;;;;** Error conditions
 (defpackage #:ch.amann-wolowyk.s-xml-error
@@ -31,7 +30,7 @@
 ;;;; class.
 (defvar s-xml::*default-dom-type* :lxml)
 
-(defun s-xml::node (tag attributes values &key (type s-xml::*default-dom-type*))
+(defun s-xml::node (tag attributes values &aux (type s-xml::*default-dom-type*))
   (case type
     (:lxml (if attributes
                (cons (cons tag attributes) values)
@@ -40,7 +39,7 @@
                    tag)))
     (:sxml (list* tag (cons :@ (oam::alist2<-plist attributes)) values))))
 
-(defun s-xml::get-tag (node &key (type s-xml::*default-dom-type*)) 
+(defun s-xml::get-tag (node &aux (type s-xml::*default-dom-type*)) 
   (case type
     (:lxml (if (consp node)
                (if (consp (car node))
@@ -49,7 +48,7 @@
                node))
     (:sxml (car node))))
 
-(defun s-xml::set-tag (node new-tag &key (type s-xml::*default-dom-type*))
+(defun s-xml::set-tag (node new-tag &aux (type s-xml::*default-dom-type*))
   (case type
     (:lxml (if (consp node)
                (if (consp (car node))
@@ -59,7 +58,7 @@
     (:sxml (rplaca node new-tag)))
   node)
 
-(defun s-xml::get-attributes (node &key (type s-xml::*default-dom-type*))
+(defun s-xml::get-attributes (node &aux (type s-xml::*default-dom-type*))
   "Return a property list representing the attributes of NODE."
   (case type
     (:lxml (when (consp node)
@@ -70,27 +69,27 @@
                                                       (eq :@ (car v)))
                                              (cdr v)))
                                        (cdr node))))))
-(defun s-xml::set-attributes (node attributes &key (type s-xml::*default-dom-type*))
+(defun s-xml::set-attributes (node attributes &aux (type s-xml::*default-dom-type*))
   (case type
     (:lxml (if (consp node)
                (rplaca node (cons (s-xml::get-tag node) attributes))
                (error 's-xml-error::immutable-node :node node)))
     (:sxml (rplacd node (cons attributes (s-xml::get-values node)))))
   node)
-(defun s-xml::add-attributes (node attributes &key (type s-xml::*default-dom-type*))
-  (let ((attrs (oam::alist<-plist (s-xml::get-attributes node :type type))))
+(defun s-xml::add-attributes (node attributes &aux (type s-xml::*default-dom-type*))
+  (let ((attrs (oam::alist<-plist (s-xml::get-attributes node))))
     (map nil #'(lambda (attr)
                  (pushnew attr attrs :key #'car))
          (oam::alist<-plist attributes))
-    (s-xml::set-attributes node (oam::plist<-alist attrs) :type type))
+    (s-xml::set-attributes node (oam::plist<-alist attrs)))
   node)
 
-(defun s-xml::get-values (node &key (type s-xml::*default-dom-type*))
+(defun s-xml::get-values (node &aux (type s-xml::*default-dom-type*))
   (case type
     (:lxml (when (consp node)
              (cdr node)))
-    (:sxml (remove :@ (cdr node) :key #'(lambda (n) (s-xml::get-tag n :type :sxml))))))
-(defun s-xml::set-values (node values &key (type s-xml::*default-dom-type*))
+    (:sxml (remove :@ (cdr node) :key #'(lambda (n) (s-xml::get-tag n))))))
+(defun s-xml::set-values (node values &aux (type s-xml::*default-dom-type*))
   (case type
     (:lxml (if (consp node)
                (rplacd node values)
@@ -98,7 +97,7 @@
     (:sxml (rplacd node (append (s-xml::get-attributes node) values))))
   node)
 (defun s-xml::insert-values (node values place
-                             &key (type s-xml::*default-dom-type*))
+                             &aux (type s-xml::*default-dom-type*))
   (case type
     (:lxml (if (consp node)
                (oam::insert* values (cdr node) place)
@@ -109,12 +108,10 @@
 ;;;;** Some node manipulation facilities
 ;;;; They (should) depend only on the API defined in previous section
 ;;;; without worrying about the effective type of the underlying dom
-;;;; format. However the key argument :type is still present. This
-;;;; should be changed.
-(defun s-xml::find-child (node child-path &key (type s-xml::*default-dom-type*))
+;;;; format.
+(defun s-xml::find-child (node child-path)
   "Return as values the child-node and its position in the list of values of NODE if this child-node exists else throw an error of type s-xml-error:node-not-found. Child-path is  a list containing the tag of the child node to find and the occurence or the tag only, in which case the occurence is 0."
-  (let* ((s-xml::*default-dom-type* type)
-         (node-values (s-xml::get-values node))
+  (let* ((node-values (s-xml::get-values node))
          child-node pos)
     (destructuring-bind (child-tag child-occurence)
         (if (consp child-path) child-path `(,child-path 0))
@@ -132,78 +129,63 @@
       (error 's-xml-error:node-not-found :parent-node node :target-tag child-path))
     (values child-node pos)))
 
-(defun s-xml::delete-child (node child-path &key (type s-xml::*default-dom-type*))
-  (let* ((s-xml::*default-dom-type* type))
-    (multiple-value-bind (child-node child-pos)
-        (s-xml::find-child node child-path)
-     (values (s-xml::set-values node (delete (s-xml::get-tag child-node)
-                                             (s-xml::get-values node)
-                                             :start child-pos :count 1
-                                             :key #'s-xml::get-tag))
-             child-pos))))
+(defun s-xml::delete-child (node child-path)
+  (multiple-value-bind (child-node child-pos)
+      (s-xml::find-child node child-path)
+    (values (s-xml::set-values node (delete (s-xml::get-tag child-node)
+                                            (s-xml::get-values node)
+                                            :start child-pos :count 1
+                                            :key #'s-xml::get-tag))
+            child-pos)))
 
-(defun s-xml::replace-child (node child-path new-child
-                             &key (type s-xml::*default-dom-type*))
-  (let* ((s-xml::*default-dom-type* type))
-    (multiple-value-bind (node child-pos)
-        (s-xml::delete-child node child-path)
-     (values (s-xml::insert-values node `(,new-child) child-pos)
-             child-pos))))
+(defun s-xml::replace-child (node child-path new-child)
+  (multiple-value-bind (node child-pos)
+      (s-xml::delete-child node child-path)
+    (values (s-xml::insert-values node `(,new-child) child-pos)
+            child-pos)))
 
-(defun s-xml::find-node (node-path xml-dom &key (type s-xml::*default-dom-type*))
+(defun s-xml::find-node (node-path xml-dom)
   (values-list (reduce #'(lambda (result node-tag)
                            (let ((parent-node (car result)))
                              (multiple-value-bind (child-node pos)
-                                 (s-xml::find-child parent-node node-tag :type type)
+                                 (s-xml::find-child parent-node node-tag)
                                (list child-node parent-node (cons pos (caddr result))))))
                        node-path
                        :initial-value (list xml-dom))))
 
 
-(defun s-xml::node-set-values (node-path values xml-dom
-                               &key (type s-xml::*default-dom-type*))
-  (let ((s-xml::*default-dom-type* type))
-    (multiple-value-bind (target parent)
-        (s-xml::find-node node-path xml-dom)
-      (handler-case
-          (s-xml::set-values target values)
-        (s-xml-error::immutable-node ()
-          (let ((target (s-xml::node (s-xml::get-tag target)
-                                     (s-xml::get-attributes target)
-                                     values)))
-            (s-xml::replace-child parent (car (last node-path)) target))))))
+(defun s-xml::node-set-values (node-path values xml-dom)
+  (multiple-value-bind (target parent)
+      (s-xml::find-node node-path xml-dom)
+    (handler-case
+        (s-xml::set-values target values)
+      (s-xml-error::immutable-node ()
+        (let ((target (s-xml::node (s-xml::get-tag target)
+                                   (s-xml::get-attributes target)
+                                   values)))
+          (s-xml::replace-child parent (car (last node-path)) target)))))
   xml-dom)
-(defun s-xml::node-insert-values (node-path values place xml-dom
-                                  &key (type s-xml::*default-dom-type*))
-  (let ((s-xml::*default-dom-type* type))
-    (multiple-value-bind (target parent)
-        (s-xml::find-node node-path xml-dom)
-      (handler-case
-          (s-xml::insert-values target values place)
-        (s-xml-error::immutable-node ()
-          (let ((target (s-xml::node (s-xml::get-tag target)
-                                     (s-xml::get-attributes target)
-                                     (oam::insert* values (s-xml::get-values target)
-                                                   place))))
-            (s-xml::replace-child parent target (car (last node-path))))))))
+(defun s-xml::node-insert-values (node-path values place xml-dom)
+  (multiple-value-bind (target parent)
+      (s-xml::find-node node-path xml-dom)
+    (handler-case
+        (s-xml::insert-values target values place)
+      (s-xml-error::immutable-node ()
+        (let ((target (s-xml::node (s-xml::get-tag target)
+                                   (s-xml::get-attributes target)
+                                   (oam::insert* values (s-xml::get-values target)
+                                                 place))))
+          (s-xml::replace-child parent target (car (last node-path)))))))
   xml-dom)
-(defun s-xml::rename-node (node-path new-name xml-dom
-                           &key (type s-xml::*default-dom-type*))
-  (let ((s-xml::*default-dom-type* type))
-    (multiple-value-bind (target parent)
-        (s-xml::find-node node-path xml-dom)
-      (handler-case
-          (s-xml::set-tag target new-name)
-        (s-xml-error::immutable-node ()
-          (let ((target (s-xml::node new-name
-                                     (s-xml::get-attributes target)
-                                     (s-xml::get-values target))))
-            (s-xml::replace-child parent target (car (last node-path))))))))
+(defun s-xml::rename-node (node-path new-name xml-dom)
+  (multiple-value-bind (target parent)
+      (s-xml::find-node node-path xml-dom)
+    (handler-case
+        (s-xml::set-tag target new-name)
+      (s-xml-error::immutable-node ()
+        (let ((target (s-xml::node new-name
+                                   (s-xml::get-attributes target)
+                                   (s-xml::get-values target))))
+          (s-xml::replace-child parent target (car (last node-path)))))))
   xml-dom)
 
-;;;;** @-xml DOM
-;;;;
-;;;; This DOM is intended to be useful as a lisp serialization of XML in for of
-;;;; s-expressions. It follows the syntax of s-xml except that the first element
-;;;; of each node is the symbol @. This symbol is bound to a macro in such a way
-;;;; that, when reading, the @-xml expression is transformed to another DOM.
