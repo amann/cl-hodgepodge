a3ba7fb4993e91b1a8b4a5de87c135d2f44dcaba
Olivier <olivier.amann@swisslife.ch>
2019-01-17 16:02:09 +0100

    REDUCE-EXPRESSION uses now "strategies."
    Strategies reduce the number of rules to use at a given time. This helps to
    guide the search process by using more relevat rules.

--- awl/src/awl-unification.lisp
+++ awl/src/awl-unification.lisp
@@ -132,24 +132,30 @@
     (depth expression 0)))
 
 
-(defclass ruleset ()
-  ((parameters :initform nil :initarg :parameters)
-   (rules :initform nil)))
+(defstruct (ruleset (:constructor %make-ruleset))
+  parameters
+  rules)
 (defvar *rulesets* (make-hash-table))
 
-(defmethod initialize-instance :after ((ruleset ruleset) &key rules)
-  (setf (slot-value ruleset 'rules)
-        (mapcan (lambda (rule)
-                  (let ((type (first rule))
-                        (rule (cons (first (rest rule))
-                                    (second (rest rule)))))
-                    (ecase type
-                      (--> (list rule))
-                      (<-> (list rule (cons (cdr rule) (car rule)))))))
-                rules)))
+(defun parse-rules (rules)
+  (let (rule-classes)
+    (dolist (rule rules rule-classes)
+      (let ((type (pop rule))
+            (rule (cons (pop rule)
+                        (pop rule)))
+            (tags (or rule (list nil))))
+        (let ((rules (ecase type
+                       (--> (list rule))
+                       (<-> (list rule (cons (cdr rule) (car rule)))))))
+          (dolist (tag tags)
+            (let ((class (assoc tag rule-classes)))
+              (if class
+                  (dolist (rule rules)
+                    (push rule (cdr class)))
+                  (push (cons tag rules) rule-classes)))))))))
 
 (defun make-ruleset (parameters rules)
-  (make-instance 'ruleset :parameters parameters :rules rules))
+  (%make-ruleset :parameters parameters :rules (parse-rules rules)))
 (defun register-ruleset (name ruleset)
   (declare (type (or null ruleset) ruleset))
   (if ruleset
@@ -157,8 +163,8 @@
       (remhash name *rulesets*)))
 
 
-(defmacro define-ruleset ((name &rest parameters) &body rules)
-  `(register-ruleset ',name (make-ruleset ',parameters ',rules)))
+(defmacro define-ruleset ((name &rest parameters) &body rules-with-tags)
+  `(register-ruleset ',name (make-ruleset ',parameters ',rules-with-tags)))
 
 
 
@@ -168,64 +174,128 @@
     (ruleset ruleset)
     ((and symbol (not null)) (gethash ruleset *rulesets*))))
 
-(defun extract-rules (ruleset &rest parameters
+
+
+(defun extract-rules (ruleset tag &rest parameters
                       &aux (ruleset (find-ruleset ruleset)))
   (etypecase ruleset
     (ruleset (mapcar (let ((bindings (delete-if (lambda (x)
                                                   (funcall awl::*equal-p* (car x) (cdr x)))
-                                                (loop :for param :in (slot-value ruleset 'parameters)
+                                                (loop :for param :in (ruleset-parameters ruleset)
                                                       :collect (cons param (or (pop parameters)
                                                                                (error "Missing parameter ~S." param)))))))
                        (lambda (rule)
                          (cons (substitute-variables bindings (car rule))
                                (substitute-variables bindings (cdr rule)))))
-                     (slot-value ruleset 'rules)))))
+                     (cdr (assoc tag (ruleset-rules ruleset)))))))
 
-(defun find-rules (ruleset &rest parameters)
-  (let ((ruleset (etypecase ruleset
-                   (ruleset ruleset)
-                   (symbol (gethash ruleset *rulesets* ruleset)))))
-    (etypecase ruleset
-      (ruleset (mapcar (let ((bindings (delete-if (lambda (x)
-                                                    (funcall awl::*equal-p* (car x) (cdr x)))
-                                                  (loop :for param :in (slot-value ruleset 'parameters)
-                                                        :collect (cons param (or (pop parameters)
-                                                                                 (error "Missing parameter ~S." param)))))))
-                         (lambda (rule)
-                           (cons (substitute-variables bindings (car rule))
-                                 (substitute-variables bindings (cdr rule)))))
-                       (slot-value ruleset 'rules))))))
+(defun find-rules (ruleset tag &rest parameters)
+  (declare (dynamic-extent parameters))
+  (let ((ruleset (find-ruleset ruleset)))
+    (apply #'extract-rules ruleset tag parameters)))
+
+
+(defun emapcar (fn list &rest more-lists)
+  (if more-lists
+      (do* (result
+            (lists (cons list more-lists))
+            (args (maplist (lambda (cons)
+                             (when (consp (car cons))
+                               (pop (car cons))))
+                           lists)
+                  (maplist (lambda (cons)
+                             (when (consp (car cons))
+                               (pop (car cons))))
+                           lists)))
+           ((every #'null args) (nreverse result))
+        (push (apply fn args) result))
+      (mapcar* fn list)))
+(defun mapcar* (fn list &rest more-lists)
+  (do (result
+       (lists (cons list more-lists)))
+      ((not (every #'consp lists)) (nreverse result))
+    (push (apply fn (maplist (lambda (cons)
+                                 (pop (car cons)))
+                               lists))
+          result)))
+
+(defun elength (expression &optional (n 0))
+  (if (consp expression)
+      (elength (cdr expression) (1+ n))
+      n))
+
+(defun expression-structure (expression)
+  (when (consp expression)
+    (cons (1- (elength expression))
+          (apply #'emapcar (lambda (&rest args)
+                             (apply '+ (delete nil args)))
+                 (mapcar* #'expression-structure expression)))))
+
+(defun expression-structure-distance (expr-struct1 expr-struct2)
+  (reduce '+ (mapcar (lambda (x)
+                       (* x x))
+                     (emapcar (lambda (x y)
+                                (- (or x 0)
+                                   (or y 0)))
+                              expr-struct1
+                              expr-struct2))))
+(defun expression-distance (expr1 expr2)
+  (expression-structure-distance (expression-structure expr1)
+                                 (expression-structure expr2)))
 
 (defvar *max-search-steps* 1000)
 (defvar *max-search-depth* 4)
-(defun reduce-expression (expr target &rest rulesets-parameters
-                          &aux (rules (mapcan (lambda (ruleset-parameters)
-                                                (apply #'extract-rules ruleset-parameters))
-                                              rulesets-parameters))
-                            (parameters (remove-duplicates (mapcan #'rest rulesets-parameters))))
+(defvar *target*)
+(defun reduce-expression (expr strategy &rest rulesets-parameters
+                          &aux
+                            (targets (mapcar 'second strategy))
+                            (strategy (mapcar 'first strategy))
+                            (rules (mapcar (lambda (tag)
+                                             (mapcan (lambda (ruleset-parameters)
+                                                       (apply #'extract-rules
+                                                              (first ruleset-parameters)
+                                                              tag
+                                                              (rest ruleset-parameters)))
+                                                     rulesets-parameters))
+                                           strategy))
+                            (parameters (delete-if 'integerp (remove-duplicates (mapcan #'rest rulesets-parameters)))))
   (let ((awl::*acceptable-p* (lambda (var expr)
                                (declare (ignore var))
                                (not (member expr parameters)))))
     (let ((search-fn (awl::a*-search-factory (lambda (node neighbor)
-                                               (1+ (- (depth neighbor)
-                                                      (depth node))))
+                                               (+ 1
+                                                  (- (depth neighbor)
+                                                     (depth node))))
                                              (lambda (expr)
-                                               (depth expr))
+                                               (* 5 (depth expr)))
                                              (lambda (expr)
-                                                (remove-duplicates (remove-if (lambda (x)
-                                                                                (< *max-search-depth* (depth x)))
-                                                                              (mapcan (lambda (rule)
-                                                                                        (apply-rule rule expr))
-                                                                                      rules))
-                                                                   :test #'equalp))
+                                               (declare (special rules))
+                                               (remove-duplicates (remove-if (lambda (x)
+                                                                               (< *max-search-depth* (depth x)))
+                                                                             (mapcan (lambda (rule)
+                                                                                       (apply-rule rule expr))
+                                                                                     rules))
+                                                                  :test #'equalp))
                                              #'equalp)))
-      (let ((step-count 0))
-        (values-list (concatenate 'list
-                                  (multiple-value-list (funcall search-fn expr (lambda (node)
-                                                                                 (incf step-count)
-                                                                                 (or (< *max-search-steps* step-count)
-                                                                                     (ignore-errors (unify node target))))))
-                                  (list step-count)))))))
+      (let ((step-count 0) costs paths foundps)
+        (dolist (rules rules (values (nreverse paths)
+                                     (nreverse costs)
+                                     (nreverse foundps)
+                                     step-count))
+          (declare (special rules))
+          (let ((*target* (pop targets)))
+           (multiple-value-bind (path cost foundp)
+               (funcall search-fn expr (lambda (node)
+                                         (declare (ignorable node))
+                                         (incf step-count)
+                                         (or (< *max-search-steps* step-count)
+                                             (when (and (boundp '*target*) *target*)
+                                               (or (ignore-errors (unify node *target*))
+                                                   (equalp node *target*))))))
+             (push path paths)
+             (push cost costs)
+             (push foundp foundps)
+             (setf expr (first (last path))))))))))
 
 
 (defun apply-rulesets (expr &rest rulesets-parameters
@@ -241,35 +311,70 @@
                                rules)
                        :test #'equalp)))
 
+(defvar *rules*)
+#+(or)
+(defun do-actions (actions expresion
+                   &aux (actions (typecase actions
+                                   (cons actions)
+                                   (t (list actions)))))
+  )
+#+(or)
+(defmacro with-rulesets ((&rest rulesets-parameters) &body body)
+  )
 
 
 
 (define-ruleset (associativity *)
-  (<-> (* a?) a?)
-  (<-> (* a? (* b? . c?))
-       (* a? b? . c?))
-  (<-> (* a? b? . c?)
-       (* (* a? b?) . c?)))
+  (--> (* a?) a? simplify)
+  (--> a? (* a?))
+  (--> (* a? (* b? . c?))
+       (* a? b? . c?) simplify)
+  (--> (* a? b? (* c? . d?))
+       (* a? b? c? . d?) simplify)
+  (--> (* a? b? c? . d?)
+       (* a? b? (* c? . d?)) simplify)
+  (--> (* a? b? . c?)
+       (* a? (* b? . c?)) simplify)
+  (--> (* (* a? b?) . c?)
+       (* a? b? . c?) simplify)
+  (--> (* a? b? . c?)
+       (* (* a? b?) . c?) simplify))
 (define-ruleset (neutral-element * 1)
-  (<-> (* 1 a?) a?)
-  (<-> (* a? 1) a?))
+  (--> (* 1 a?) a? simplify)
+  (--> a? (* 1 a?))
+  (--> (* a? 1) a? simplify)
+  (--> a? (* a? 1))
+  (--> (*) 1 simplify))
 (define-ruleset (inverse-element * / 1)
-  (<-> (* (/ a?) a?) 1)
-  (<-> (* a? (/ a?)) 1))
+  (--> (* (/ a?) a?) 1 simplify)
+  (--> (* a? (/ a?)) 1 simplify))
+
+(define-ruleset (opposite-operation * /)
+  (--> (* a? (/ (* b? . c?))) (/ a? b? . c?) simplify)
+  (--> (/ a? b? . c?) (* a? (/ (* b? . c?))) simplify expand))
+
 (define-ruleset (commutativity +)
   (<-> (+ a? b?) (+ b? a?)))
 
 (define-ruleset (absorbing-element * 0)
-  (<-> (* 0 a?) 0)
-  (<-> (* a? 0) 0))
+  (--> (* 0 a?) 0 simplify)
+  (--> (* a? 0) 0 simplify))
 (define-ruleset (distributivity * +)
-  (<-> (* a? (+ b? c?))
-       (+ (* a? b?) (* a? c?)))
-  (<-> (* (+ b? c?) . a?)
-       (+ (* b? . a?) (* c? . a?))))
+  (--> (* a? (+ b? c?))
+       (+ (* a? b?) (* a? c?)) expand)
+  (--> (+ (* a? b?) (* a? c?))
+       (* a? (+ b? c?)) factorize)
+  (--> (* (+ b? c?) a?)
+       (+ (* b? a?) (* c? a?)) expand)
+  (--> (+ (* b? a?) (* c? a?))
+       (* (+ b? c?) a?) factorize))
 
 
-
+(define-ruleset (opposite-action * - 1)
+  (--> (* (- 1) a?) (- a?) simplify)
+  (--> (- a?) (* (- 1) a?))
+  (--> (* a? (- 1)) (- a?) simplify)
+  (--> (- a?) (* a? (- 1))))
 
 
 
